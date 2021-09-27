572469f86d66ecf4d59cd918a9bfc64f555965e5
Joris Scharp <jorisscharp@gmail.com>
2021-09-27 15:09:38 +0200

    Initial Spring initializr setup

---
+++ src/test/java/nl/koppeltaal/poc/module/ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.koppeltaal.poc.module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
